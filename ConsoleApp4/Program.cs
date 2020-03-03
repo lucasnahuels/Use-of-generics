9cd77a3879f310f56ab2cf06dbfd5ec1b684b6b2
Lucas Stirpe <v-lstirpe@youngliving.com>
2020-03-03 17:03:29 -0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(factorial_Recursion(4));
+            var repo = new RepositoryBank();
+            var cuenta = repo.Get(1);
+            //cuenta. no contiene sus propiedades
+            repo.Save(new BankAccount());
+            var repo2 = new Repository<BankAccount>();
+            var cuenta2 = repo2.Get(1);
+            //cuenta2.NumeroDeCuenta si lo contiene
+            var repo3 = new Repository<Customer>();
+            var custom = repo3.Get(1);
+        }
+        class RepositoryBank
+        {
+            public virtual ISerializable Get(int id)
+            {
+                return new BankAccount();
+            }
+            public void Save(ISerializable obj)
+            {
+            }
+            public void Delete(ISerializable obj)
+            {
+            }
+        }
+        class RepositoryCustomer : RepositoryBank
+        {
+            public override ISerializable Get(int id)
+            {
+                return new Customer();
+            }
+        }
+        class Repository<T> where T : ISerializable
+        {
+            List<T> source = new List<T>();
+            public T Get(int id)
+            {
+                return source.FirstOrDefault();
+            }
+            public void Save(T obj)
+            {
+            }
+            public void Delete(T obj)
+            {
+            }
+        }
+        class BankAccount : ISerializable
+        {
+            public int NumeroDeCuenta { get; set; }
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class Customer : ISerializable
+        {
+            public int NumeroDeCliente { get; set; }
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public static double factorial_Recursion(int number)
+        {
+            if (number == 1)
+                return 1;
+            else
+                return number * factorial_Recursion(number - 1);
+        }
+    }
+}
